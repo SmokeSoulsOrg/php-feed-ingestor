5a5efebc94704169c4bf54a21a39630361aaefee
Elias Papa <elias.papa92@gmail.com>
2025-07-05 19:23:35 +0300

    Trying to fix issues when orchestrating from infra-deployment.

--- config/queue.php
+++ config/queue.php
@@ -40,7 +40,7 @@ return [
             'port' => env('RABBITMQ_PORT', 5672),
             'username' => env('RABBITMQ_USER', 'guest'),
             'password' => env('RABBITMQ_PASSWORD', 'guest'),
-            'queue' => env('RABBITMQ_QUEUE', 'default'),
+            'queue' => env('RABBITMQ_QUEUE', 'image-download,pornstar-events'),
         ],
 
         'database' => [
--- config/services.php
+++ config/services.php
@@ -20,7 +20,7 @@ return [
         'port' => env('RABBITMQ_PORT', 5672),
         'user' => env('RABBITMQ_USER', 'guest'),
         'password' => env('RABBITMQ_PASSWORD', 'guest'),
-        'queue' => env('RABBITMQ_QUEUE', 'default'),
+        'queue' => env('RABBITMQ_QUEUE', 'image-download,pornstar-events'),
     ],
 
     'api' => [
